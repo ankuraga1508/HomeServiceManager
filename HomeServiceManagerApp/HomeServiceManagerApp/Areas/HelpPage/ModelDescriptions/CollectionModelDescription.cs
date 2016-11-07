a1c5fc6d0cc2730e32995bf5819741c0e74a701c
arjunb@iastate.edu <arjunb@iastate.edu>
2016-11-06 22:08:50 -0600

    Initial web project

---
+++ HomeServiceManagerApp/HomeServiceManagerApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HomeServiceManagerApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
